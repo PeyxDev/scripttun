cb7ea3e1de53b5278862dd24436b0a0f2b87fa18
PEYX <peyxdev@gmail.com>
2026-07-31 15:20:47 +0700

    Add files via upload

--- project/openvpn/vpn.sh
+++ project/openvpn/vpn.sh
@@ -5,7 +5,7 @@ echo "✨ FILE ENC BY PeyxDev"
 # By PX VPN 
 # ==================================================
 # Link Hosting Kalian
-REPO="https://raw.githubusercontent.com/PeyxDev/scrippttun/main/"
+REPO="https://raw.githubusercontent.com/PeyxDev/scripttun/main/"
 
 # initialisasi var
 export DEBIAN_FRONTEND=noninteractive
